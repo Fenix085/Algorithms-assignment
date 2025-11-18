8b6c75f0987b2958bb367360bc39817390831d09
Volodymyr <79228320+Fenix085@users.noreply.github.com>
2025-11-18 11:10:59 +0100

    feat: add merge sort + return values, update main
    Bubble returns array now
    Insertion also returns sorted list
    Merge sort and merge added

--- main_task_1.py
+++ main_task_1.py
@@ -6,7 +6,8 @@ def bubbleSort(arr):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-
+    return arr
+# ----------------------------------------------------------------
 def insertionSort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
@@ -15,11 +16,45 @@ def insertionSort(arr):
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
+    return arr
+# ----------------------------------------------------------------
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    lHalf = arr[:mid]
+    rHalf = arr[mid:]
+
+    sorted_l = mergeSort(lHalf)
+    sorted_r = mergeSort(rHalf)
+
+    return merge(sorted_l, sorted_r)
+
+def merge(l, r):
+    result = []
+    i = j = 0
+    while i < len(l) and j < len(r):
+        if l[i] < r[j]:
+            result.append(l[i])
+            i += 1
+        else:
+            result.append(r[j])
+            j += 1
+
+    result.extend(l[i:])
+    result.extend(r[j:])
+
+    return result
+
+# ----------------------------------------------------------------
+
 
 if __name__ == "__main__":
     arr = [64, 34, 25, 12, 22, 11, 90]
 
-    # bubbleSort(arr)
-    insertionSort(arr)
-
-    print("Sorted array:", arr)
+    fin_arr = bubbleSort(arr.copy())
+    print("Sorted array:", fin_arr)
+    fin_arr = insertionSort(arr.copy())
+    print("Sorted array:", fin_arr)
+    fin_arr = mergeSort(arr.copy())
+    print("Sorted array:", fin_arr)
