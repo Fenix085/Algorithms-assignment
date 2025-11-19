527092c50e44c00652b1f7fa50d1217596f6a30e
Volodymyr <79228320+Fenix085@users.noreply.github.com>
2025-11-19 15:37:57 +0100

    refactor: sorting funcs into Sorter class
    Sort implementations now live
    inside a Sorter class with methods
    for bubble, insert, merge, quick

--- main_task_1.py
+++ main_task_1.py
@@ -1,77 +1,81 @@
 #task one: implementation of bubble sort, insertion sort, merge sort and quick sort
 import random
 
-def bubbleSort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
-# ----------------------------------------------------------------
-def insertionSort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    return arr
-# ----------------------------------------------------------------
-def mergeSort(arr):
-    if len(arr) <= 1:
+class Sorter:
+    def __init__(self):
+        pass
+
+    def bubbleSort(self, arr):
+        n = len(arr)
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
         return arr
-    mid = len(arr) // 2
-    lHalf = arr[:mid]
-    rHalf = arr[mid:]
-
-    sorted_l = mergeSort(lHalf)
-    sorted_r = mergeSort(rHalf)
-
-    return merge(sorted_l, sorted_r)
-
-def merge(l, r):
-    result = []
-    i = j = 0
-    while i < len(l) and j < len(r):
-        if l[i] < r[j]:
-            result.append(l[i])
-            i += 1
-        else:
-            result.append(r[j])
-            j += 1
-
-    result.extend(l[i:])
-    result.extend(r[j:])
-
-    return result
-
 # ----------------------------------------------------------------
+    def insertionSort(self, arr):
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and key < arr[j]:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+        return arr
+    # ----------------------------------------------------------------
+    def mergeSort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        mid = len(arr) // 2
+        lHalf = arr[:mid]
+        rHalf = arr[mid:]
 
-def quickSort(arr, l = 0, r = None):
-    if r is None:
-        r = len(arr) - 1
-    
-    if l < r:
-        pivot_index = random.randint(l, r)
-        arr[pivot_index], arr[r] = arr[r], arr[pivot_index]
+        sorted_l = self.mergeSort(lHalf)
+        sorted_r = self.mergeSort(rHalf)
+
+        return self.merge(sorted_l, sorted_r)
+
+    def merge(self, l, r):
+        result = []
+        i = j = 0
+        while i < len(l) and j < len(r):
+            if l[i] < r[j]:
+                result.append(l[i])
+                i += 1
+            else:
+                result.append(r[j])
+                j += 1
+
+        result.extend(l[i:])
+        result.extend(r[j:])
+
+        return result
+
+    # ----------------------------------------------------------------
+
+    def quickSort(self, arr, l = 0, r = None):
+        if r is None:
+            r = len(arr) - 1
+        
+        if l < r:
+            pivot_index = random.randint(l, r)
+            arr[pivot_index], arr[r] = arr[r], arr[pivot_index]
+            pivot = arr[r]
+            pivot = self.partition(arr, l, r)
+            self.quickSort(arr, l, pivot - 1)
+            self.quickSort(arr, pivot + 1, r)
+        return arr
+
+    def partition(self, arr, l, r):
         pivot = arr[r]
-        pivot = partition(arr, l, r)
-        quickSort(arr, l, pivot - 1)
-        quickSort(arr, pivot + 1, r)
-    return arr
+        i = l - 1
+        for j in range(l, r):
+            if arr[j] <= pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
 
-def partition(arr, l, r):
-    pivot = arr[r]
-    i = l - 1
-    for j in range(l, r):
-        if arr[j] <= pivot:
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-
-    arr[i + 1], arr[r] = arr[r], arr[i + 1]
-    return i + 1
+        arr[i + 1], arr[r] = arr[r], arr[i + 1]
+        return i + 1
 
 
 
